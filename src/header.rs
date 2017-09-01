6962c22066e101dc21d3b731781036fcd4e21001
dignifiedquire <dignifiedquire@gmail.com>
2017-09-02 01:28:49 +0200

    simple header parsing works

--- src/header.rs
+++ src/header.rs
@@ -1,5 +1,5 @@
-use std::fmt;
-// use std::collections::HashMap;
+use std::{fmt, str};
+use std::collections::HashMap;
 
 /// What type of key is used.
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -19,6 +19,18 @@ impl fmt::Display for KeyType {
     }
 }
 
+impl str::FromStr for KeyType {
+    // TODO: proper error type
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "1" => Ok(KeyType::OpenPGP),
+            _ => Ok(KeyType::Unknown(s.to_string())),
+        }
+    }
+}
+
 /// Possible values for encryption preference
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub enum EncryptPreference {
@@ -29,6 +41,18 @@ impl fmt::Display for EncryptPreference {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             EncryptPreference::Mutual => write!(fmt, "mutual"),
+        }
+    }
+}
+
+impl str::FromStr for EncryptPreference {
+    // TODO: proper error type
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "mutual" => Ok(EncryptPreference::Mutual),
+            _ => Err(()),
         }
     }
 }
@@ -77,21 +101,68 @@ impl Header {
     }
 }
 
+impl str::FromStr for Header {
+    // TODO: proper error type
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let attributes: HashMap<&str, &str> = s.split(";")
+            .filter_map(|a| {
+                            let attribute: Vec<&str> = a.trim().split("=").collect();
+                            if attribute.len() == 2 {
+                                Some((attribute[0], attribute[1]))
+                            } else {
+                                None
+                            }
+                        })
+            .collect();
+
+        if let Some(addr) = attributes.get("addr") {
+            if let Some(keydata) = attributes.get("keydata") {
+                let typ = attributes.get("type").and_then(|s| s.parse().ok());
+
+                let pref = attributes
+                    .get("prefer-encrypt")
+                    .and_then(|s| s.parse().ok());
+
+                return Ok(Header::new(addr.to_string(), typ, pref, keydata.to_string()));
+            }
+        }
+
+        Err(())
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn keydata() -> String {
+        "mDMEWFUX7RYJKwYBBAHaRw8BAQdACHq6FkRGsHqBMsNpD7d+Q2jtxVwTO+Y4NhBaQyHaMj+0HWFsaWNlQHRlc3RzdWl0ZS5hdXRvY3J5cHQub3JniJAEExYIADgWIQQmqmdR/XZoxC+kkkr8dE2p/nPD1AUCWFUX7QIbAwULCQgHAgYVCAkKCwIEFgIDAQIeAQIXgAAKCRD8dE2p/nPD1EqOAP0WUDKwko001X7XTSYbWGWmXfR9P1Aw6917EnkVQMsp3gEA86Ii8ArL3jd+E2qS5JSysx/qiVhuTSwWzmC5K6zKdg+4OARYVRfuEgorBgEEAZdVAQUBAQdAv1A88FoCfwz0zSh6NNnUuKuz1p3ctJ3kXMGotsVYjA0DAQgHiHgEGBYIACAWIQQmqmdR/XZoxC+kkkr8dE2p/nPD1AUCWFUX7gIbDAAKCRD8dE2p/nPD1FTOAP4nS14sX7a/nBXBKWAh/oX8iVtkhmZqjy9tG21BcNqb+wEAq73H4+1ncnkscR3Nu4GYzNRSD3NXq68tEESK28kYvw4=".to_string()
+    }
+
     #[test]
     fn test_fmt() {
-        let keydata = "mDMEWFUX7RYJKwYBBAHaRw8BAQdACHq6FkRGsHqBMsNpD7d+Q2jtxVwTO+Y4NhBaQyHaMj+0HWFsaWNlQHRlc3RzdWl0ZS5hdXRvY3J5cHQub3JniJAEExYIADgWIQQmqmdR/XZoxC+kkkr8dE2p/nPD1AUCWFUX7QIbAwULCQgHAgYVCAkKCwIEFgIDAQIeAQIXgAAKCRD8dE2p/nPD1EqOAP0WUDKwko001X7XTSYbWGWmXfR9P1Aw6917EnkVQMsp3gEA86Ii8ArL3jd+E2qS5JSysx/qiVhuTSwWzmC5K6zKdg+4OARYVRfuEgorBgEEAZdVAQUBAQdAv1A88FoCfwz0zSh6NNnUuKuz1p3ctJ3kXMGotsVYjA0DAQgHiHgEGBYIACAWIQQmqmdR/XZoxC+kkkr8dE2p/nPD1AUCWFUX7gIbDAAKCRD8dE2p/nPD1FTOAP4nS14sX7a/nBXBKWAh/oX8iVtkhmZqjy9tG21BcNqb+wEAq73H4+1ncnkscR3Nu4GYzNRSD3NXq68tEESK28kYvw4=";
-
         let h = Header::new("me@mail.com".to_string(),
                             Some(KeyType::OpenPGP),
                             Some(EncryptPreference::Mutual),
-                            keydata.to_string());
+                            keydata());
 
         assert_eq!(h.to_string(),
                    format!("addr=me@mail.com; type=1; prefer-encrypt=mutual; keydata={}",
-                           keydata));
+                           keydata()));
+    }
+
+    #[test]
+    fn test_from_str() {
+        let h: Header = "addr=me@mail.com; type=1; prefer-encrypt=mutual; keydata=mykey"
+            .parse()
+            .expect("failed to parse");
+
+        assert_eq!(h.addr, "me@mail.com");
+        assert_eq!(h.typ, Some(KeyType::OpenPGP));
+        assert_eq!(h.prefer_encrypt, Some(EncryptPreference::Mutual));
+        assert_eq!(h.keydata, "mykey");
     }
 }
